0062d07ae2195b1b94c7a5cf1fcc3d4b35d98c9e
Valentin DELAPRE <valentin.delapre@gmail.com>
2018-02-01 23:23:46 +0100

    Ebauche menu
    Définition de la taille
    Bordures
    Mises en place des titres
    
    Couleurs à changer
    Font à changer
    Inté JS à faire
    Finir le css

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+	<link rel="stylesheet" href="assets/css/style.css">
+</head>
+<body>
+
+	<div class="container"> 
+		
+		<!--Side Menu -->
+		<?php 
+		require 'menu.php'
+		?>
+
+	</div>
+	
+</body>
+</html>
---
+++ menu.php
@@ -0,0 +1,8 @@
+<aside>
+	<ul>
+			<li><a href="#">Partie en cours</a></li>
+			<li><a href="#">Informations du joueur</a></li>
+			<li><a href="#">Liste et stats des champions</a></li>
+			<li><a href="#">Guides</a></li>
+		</ul>
+</aside>		
