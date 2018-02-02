f46d825bc1f65bdccc0a23890830f85512f91e78
Valentin DELAPRE <valentin.delapre@gmail.com>
2018-02-02 12:04:26 +0100

    Boostrap test

--- menu.php
+++ menu.php
@@ -1,8 +1,7 @@
-<aside>
-	<ul>
-			<li><a href="#">Partie en cours</a></li>
-			<li><a href="#">Informations du joueur</a></li>
-			<li><a href="#">Liste et stats des champions</a></li>
-			<li><a href="#">Guides</a></li>
-		</ul>
+<aside class="list-group" id="menu">
+	<a class="list-group-item list-group-item-action" href="#">Partie en cours</a>
+	<a class="list-group-item list-group-item-action" href="#">Informations du joueur</a>
+	<a class="list-group-item list-group-item-action" href="#">Liste et stats des champions</a>
+	<a class="list-group-item list-group-item-action" href="#">Guides</a>
+
 </aside>		
---
+++ search.php
@@ -0,0 +1,19 @@
+<form action="search.php" class="search ">
+
+	<input type="text" name="search" id="search" placeholder="Summoner's Name" class="form-control col-lg-8">
+	<select name="region" id="region" class="col-lg-1">
+		<option value="NA">NA</option>
+		<option value="EUW">EUW</option>
+		<option value="KR">KR</option>
+		<option value="EUN">EUN</option>
+		<option value="OC">OC</option>
+		<option value="LAN">LAN</option>
+		<option value="BR">BR</option>
+		<option value="LAS">LAS</option>
+		<option value="LAS">LAS</option>
+		<option value="JA">JA</option>
+		<option value="RU">RU</option>
+	</select>
+	<button class="btn btn-primary">Voir !</button>
+
+</form>
